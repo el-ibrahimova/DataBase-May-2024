c2576807acbf08a60abb0a0378f1f5f27a8bf9c8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-07 20:42:13 +0300

    update

--- 02. CRUD/SoftUniLab-Database.sql
+++ 02. CRUD/SoftUniLab-Database.sql
@@ -5,3 +5,11 @@ WHERE DepartmentID =1
 SELECT FirstName, LastName, Salary, d.[Name]
 FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
 WHERE e.DepartmentID =1
+
+SELECT 
+FirstName AS [Име]
+,LastName AS [Фамилия]
+,Salary AS [Заплата]
+,d.[Name] AS [Отдел]
+FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
+WHERE e.DepartmentID =1

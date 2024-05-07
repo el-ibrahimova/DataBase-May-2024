29a8b565721076ad3603195d07f13026560afa33
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-07 22:07:39 +0300

    update

--- 02. CRUD/SoftUniLab-Database.sql
+++ 02. CRUD/SoftUniLab-Database.sql
@@ -1,11 +1,11 @@
 SELECT FirstName, LastName, Salary 
 FROM Employees 
-WHERE DepartmentID =1
+WHERE DepartmentID =1;
 
 -- свързване на две таблици по обща колона DepartmentID
 SELECT FirstName, LastName, Salary, d.[Name]
 FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
-WHERE e.DepartmentID =1
+WHERE e.DepartmentID =1;
 
 -- изписване на имената на колоните на български език
 SELECT 
@@ -14,7 +14,7 @@ SELECT
 	,Salary AS [Заплата]
 	,d.[Name] AS [Отдел]
 FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
-WHERE e.DepartmentID =1
+WHERE e.DepartmentID =1;
 
 -- сортиране на данните от таблицата по големината на заплатата
 SELECT 
@@ -24,7 +24,7 @@ SELECT
 	,d.[Name] AS [Отдел]
 FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
 WHERE e.DepartmentID =1
-ORDER BY Salary DESC
+ORDER BY Salary DESC;
 
 -- избиране на човека с най-високата заплата
 SELECT TOP 1
@@ -34,7 +34,7 @@ SELECT TOP 1
 	,d.[Name] AS [Отдел]
 FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
 WHERE e.DepartmentID =1
-ORDER BY Salary DESC
+ORDER BY Salary DESC;
 
 -- сливаме името, презимето и фамилията на човека в една колона
 SELECT 
@@ -45,4 +45,30 @@ SELECT
 	,d.[Name] AS [Отдел]
 FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
 WHERE e.DepartmentID =1
-ORDER BY Salary DESC
+ORDER BY Salary DESC;
+
+-- свързваме още една таблица Towns и изписваме колоната с името на града
+SELECT 
+	CONCAT_WS(' ', FirstName, MiddleName, LastName) AS [Име]
+	,Salary AS [Заплата]
+	,d.[Name] AS [Отдел]
+	,t.[Name] AS [Град]
+FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Towns AS t ON a.TownID = t.TownID
+WHERE d.[Name] = 'Engineering'
+ORDER BY Salary DESC;
+
+-- записваме кода със заявката под името v_EngineeringEmployeesBySalary, за да може да го преизползваме 
+CREATE VIEW v_EngineeringEmployeesBySalary AS SELECT 
+	CONCAT_WS(' ', FirstName, MiddleName, LastName) AS [Име]
+	,Salary AS [Заплата]
+	,d.[Name] AS [Отдел]
+	,t.[Name] AS [Град]
+FROM Employees AS e JOIN Departments AS d ON e.DepartmentID=d.DepartmentID
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Towns AS t ON a.TownID = t.TownID
+WHERE d.[Name] = 'Engineering';
+
+
+SELECT * FROM v_EngineeringEmployeesBySalary;

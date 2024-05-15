06abc2988c4f504bee4f81d9c638a153c5ddd8aa
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-15 23:28:28 +0300

    Create 03. Table Relations - Exercise.sql

---
+++ 03. Table Relations/03. Table Relations - Exercise.sql
@@ -0,0 +1,7 @@
+
+-- 09. Peaks in Rila 
+SELECT m.MountainRange, p.PeakName, p.Elevation
+FROM Mountains AS m
+JOIN Peaks AS p ON p.MountainId = m.Id
+WHERE m.MountainRange = 'Rila'
+ORDER BY p.Elevation DESC

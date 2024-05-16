f7b7e001c859c621750ddf860000c517ee696229
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-16 22:40:56 +0300

    update

--- 03. Table Relations/01. One-To-One Relationship.sql
+++ 03. Table Relations/01. One-To-One Relationship.sql
@@ -28,4 +28,10 @@ INSERT INTO Passports (PassportNumber)
      VALUES
 			('N34FG21B'),
 			('K65LO4R7'),
-			('ZE657QP2')
+			('ZE657QP2');
+
+INSERT INTO Persons (FirstName, Salary, PassportID)
+     VALUES
+			('Roberto', 43300, 102),
+			('Tom', 56100, 103),
+			('Yana', 60200, 101);

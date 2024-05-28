fc005f3e4550c511ab24ae6fe006c7e2eb965729
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-28 20:27:10 +0300

    Create 07. Employees With Project.sql

---
+++ 05. Subqueries and Joins/07. Employees With Project.sql
@@ -0,0 +1,9 @@
+SELECT TOP 5
+	 e.EmployeeID,
+	 e.FirstName,
+	 p.[Name] AS ProjectName
+FROM EmployeesProjects AS ep
+JOIN Employees AS e ON ep.EmployeeID = e.EmployeeID
+JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13' AND p.EndDate IS NULL
+ORDER BY e.EmployeeID

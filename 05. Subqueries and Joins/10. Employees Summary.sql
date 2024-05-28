8697521e645190fc473c62ed6aa3e9029d28341a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-28 22:42:44 +0300

    Create 10. Employees Summary.sql

---
+++ 05. Subqueries and Joins/10. Employees Summary.sql
@@ -0,0 +1,10 @@
+	  SELECT 
+			TOP 50
+            e.EmployeeID,
+		    CONCAT_WS(' ', e.FirstName, e.LastName) AS EmployeeName,
+			CONCAT_WS(' ', m.FirstName, m.LastName) AS ManagerName,
+          d.[Name] AS DepartmentName
+     FROM Employees AS e 
+LEFT JOIN Employees AS m ON e.ManagerID = m.EmployeeID 
+LEFT JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+ ORDER BY e.EmployeeID

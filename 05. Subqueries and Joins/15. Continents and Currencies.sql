d5037db24a95ac36dc5c247643e47b7e96e7c213
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-29 23:33:43 +0300

    Update 15. Continents and Currencies.sql

--- 05. Subqueries and Joins/15. Continents and Currencies.sql
+++ 05. Subqueries and Joins/15. Continents and Currencies.sql
@@ -1,3 +1,4 @@
+-- method 1
 SELECT ContinentCode,
        CurrencyCode, 
 	   CurrencyUsage
@@ -18,3 +19,19 @@ SELECT ContinentCode,
  WHERE CurrencyRank=1
 
 
+ -- method 2
+ SELECT rc.ContinentCode,
+        rc.CurrencyCode,
+        rc.Count
+   FROM
+   (
+	 SELECT 
+	         c.ContinentCode,
+	         c.CurrencyCode,
+	   COUNT (c.CurrencyCode) AS Count,
+	   DENSE_RANK ()
+	      OVER (PARTITION BY c.ContinentCode ORDER BY COUNT (c.CurrencyCode) DESC) AS [rank]
+	 FROM Countries AS c
+	 GROUP BY c.ContinentCode, c.CurrencyCode
+    )  AS rc
+ WHERE rc.[rank] =1 AND rc.Count >1

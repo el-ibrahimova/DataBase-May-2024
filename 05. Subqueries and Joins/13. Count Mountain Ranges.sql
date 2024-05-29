87c9a2fb7049a2f3d366dfda11a71141ba9537de
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-05-29 23:06:16 +0300

    Create 13. Count Mountain Ranges.sql

---
+++ 05. Subqueries and Joins/13. Count Mountain Ranges.sql
@@ -0,0 +1,10 @@
+SELECT CountryCode,
+ COUNT (MountainId) AS MountainRanges
+  FROM MountainsCountries
+ WHERE CountryCode IN 
+				(
+				SELECT CountryCode
+				  FROM Countries
+				 WHERE CountryName IN ('United States', 'Russia', 'Bulgaria')
+				)
+GROUP BY CountryCode

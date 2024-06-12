6fae48895dc4884104940175080a265cb7c28325
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-12 08:37:43 +0300

    Create 09. Tourist booked in Hotels.sql

---
+++ Exam preparation 15 Oct 2023/09. Tourist booked in Hotels.sql
@@ -0,0 +1,9 @@
+SELECT 
+	h.[Name] AS HotelName,
+	r.Price AS RoomPrice
+FROM Tourists AS t
+JOIN Bookings AS b ON t.Id = b.TouristId
+JOIN Hotels AS h ON h.Id = b.HotelId
+JOIN Rooms AS r ON b.RoomId = r.Id
+WHERE t.[Name] NOT LIKE '%EZ'
+ORDER BY r.Price DESC

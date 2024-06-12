42715d97f7933d17af4a5ba1e35364d1fe47edbe
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-12 08:37:31 +0300

    Create 06. Hotels by Count of Bookings.sql

---
+++ Exam preparation 15 Oct 2023/06. Hotels by Count of Bookings.sql
@@ -0,0 +1,9 @@
+SELECT 
+h.Id, h.[Name]
+FROM Hotels AS h
+JOIN HotelsRooms AS hr ON h.Id = hr.HotelId
+JOIN Rooms AS r ON r.Id = hr.RoomId
+JOIN Bookings AS b ON h.Id = b.HotelId 
+WHERE r.[Type] = 'VIP Apartment' 
+GROUP BY h.Id, h.[Name]
+ORDER BY COUNT (*) DESC

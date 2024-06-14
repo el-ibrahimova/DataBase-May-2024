6778d9f9c548cfadd61035fe212749b54a8cc719
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-14 09:04:28 +0300

    Create 04. Delete.sql

---
+++ Exam Preparation 05 Dec 2023/04. Delete.sql
@@ -0,0 +1,19 @@
+BEGIN TRANSACTION
+
+DELETE 
+FROM TrainsRailwayStations
+WHERE TrainId =7;
+
+DELETE 
+FROM Tickets
+WHERE TrainId =7;
+
+DELETE 
+FROM MaintenanceRecords
+WHERE TrainId =7;
+
+DELETE 
+FROM Trains 
+WHERE DepartureTownId IN (SELECT Id FROM Towns WHERE [Name] = 'Berlin')
+
+ROLLBACK TRANSACTION
